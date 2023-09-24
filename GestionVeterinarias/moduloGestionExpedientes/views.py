fd39b287285a53779047867c5688e42ee72279a8
pv17028 <pv17028@ues.edu.sv>
2023-09-24 16:11:20 -0600

    Permisos de usuario

--- GestionVeterinarias/moduloGestionExpedientes/views.py
+++ GestionVeterinarias/moduloGestionExpedientes/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-from GestionVeterinarias.decorators import dueño_required
+from GestionVeterinarias.decorators import no_admin_allowed
 from .models import Expediente
 from .forms import ExpedienteForm
 from django.contrib import messages
 
-@dueño_required
+@no_admin_allowed
 def lista_expedientes(request):
     expedientes = Expediente.objects.all()
     return render(request, "lista_expedientes.html", {"expedientes": expedientes})
 
-@dueño_required
+@no_admin_allowed
 def crear_expediente(request):
     if request.method == "POST":
         form = ExpedienteForm(request.POST, request.FILES)
@@ -25,7 +25,7 @@ def crear_expediente(request):
         form = ExpedienteForm()
     return render(request, "crear_expediente.html", {"form": form})
 
-@dueño_required
+@no_admin_allowed
 def actualizar_expediente(request, pk):
     expediente = get_object_or_404(Expediente, pk=pk)
     if request.method == "POST":
@@ -41,13 +41,13 @@ def actualizar_expediente(request, pk):
         form = ExpedienteForm(instance=expediente)
     return render(request, "editar_expediente.html", {"form": form, "expediente": expediente})
 
-@dueño_required
+@no_admin_allowed
 def eliminar_expediente(request, pk):
     expediente = get_object_or_404(Expediente, pk=pk)
     expediente.delete()
     return redirect("moduloGestionExpedientes:lista_expedientes")
 
-@dueño_required
+@no_admin_allowed
 def ver_expediente(request, pk):
     expediente = get_object_or_404(Expediente, pk=pk)
     return render(request, "detalle_expediente.html", {"expediente": expediente})
--- GestionVeterinarias/moduloSeguridad/admin.py
+++ GestionVeterinarias/moduloSeguridad/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from moduloSeguridad.models import CustomUser
 
-# Register your models here.
+admin.site.register(CustomUser)
